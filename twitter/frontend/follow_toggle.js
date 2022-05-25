655af896ceafde1b772da172667472bd867073c4
apodavonov3616 <apodavonov3616@DESKTOP-DU4IAMR.localdomain>
2022-05-25 15:08:58 -0400

    created click handler

--- twitter/frontend/follow_toggle.js
+++ twitter/frontend/follow_toggle.js
@@ -3,17 +3,33 @@ class FollowToggle {
         this.$el = $(el);
         this.userId = this.$el.data('user-id')
         this.followState = (this.$el.data('inital-follow-state'))
+        this.render()
     }
-}
 
-FollowToggle.prototype.render = function () {
-    if (this.followState === 'unfollowed') {
-        this.$el.prop('disabled', false);
-        this.$el.html('Follow!')
-    } else if (this.followState === 'followed') {
+    render () {
+        if (this.followState === 'unfollowed') {
             this.$el.prop('disabled', false);
-            this.$el.html('Unfollow!')
+            this.$el.html('Follow!')
+        } else if (this.followState === 'followed') {
+                this.$el.prop('disabled', false);
+                this.$el.html('Unfollow!')
+            }
         }
+
+    handleClick(something) {
+        let followToggle = this
+        something.preventDefault();
+        if (this.followState === 'followed') {
+            this.followState = 'unfollowing';
+            this.render()
+        } else if (this.followState = 'unfollowed') {
+            this.followState = 'following';
+            this.render();
+        }
+
     }
+
+        
+}
 
 module.exports = FollowToggle
