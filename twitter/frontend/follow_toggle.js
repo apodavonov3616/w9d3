1d99bb7bd9f3fa8d7c820b9cf9b6d2555aa44eef
apodavonov3616 <apodavonov3616@DESKTOP-DU4IAMR.localdomain>
2022-05-25 14:58:43 -0400

    created toggle render

--- twitter/frontend/follow_toggle.js
+++ twitter/frontend/follow_toggle.js
@@ -1,9 +1,19 @@
 class FollowToggle {
-    contructor (el) {
-        this.el = $(el);
-        this.userId = this.el.data('user-id')
-        this.followState = (this.el.data('inital-follow-state'))
+    contructor ($el) {
+        this.$el = $(el);
+        this.userId = this.$el.data('user-id')
+        this.followState = (this.$el.data('inital-follow-state'))
     }
 }
+
+FollowToggle.prototype.render = function () {
+    if (this.followState === 'unfollowed') {
+        this.$el.prop('disabled', false);
+        this.$el.html('Follow!')
+    } else if (this.followState === 'followed') {
+            this.$el.prop('disabled', false);
+            this.$el.html('Unfollow!')
+        }
+    }
 
 module.exports = FollowToggle
