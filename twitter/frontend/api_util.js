8c9211c82d45f1ed2683e31a0b9baeb1db870af4
apodavonov3616 <apodavonov3616@DESKTOP-DU4IAMR.localdomain>
2022-05-25 16:36:04 -0400

    wrapping up userscontroller

--- twitter/frontend/api_util.js
+++ twitter/frontend/api_util.js
@@ -7,6 +7,15 @@ const APIUtil = {
         APIUtil.changeFollowStatus(id, 'DELETE')
     },
   
+    searchUsers: query => (
+        $.ajax({
+          url: '/users/search',
+          dataType: 'json',
+          method: 'GET',
+          data: { query }
+        })
+      ),
+
     }
   
 module.exports = APIUtil;
---
+++ twitter/frontend/users_search.js
@@ -0,0 +1,5 @@
+class UsersSearch {
+    constructor(el) {
+        this.$el = $(el) 
+    }
+}
